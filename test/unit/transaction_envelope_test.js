cbd0d2f6be3a16e26b234916e14ccb3c79a7cd27
masoud moghini <koosha_pooyeshgar@outlook.com>
2020-06-16 19:05:51 +0430

    change one test

--- test/unit/transaction_envelope_test.js
+++ test/unit/transaction_envelope_test.js
@@ -17,10 +17,10 @@ describe('TransactionEnvelope', function() {
   it('calculates correct hash with non-utf8 strings', function(done) {
     // a84d534b3742ad89413bdbf259e02fa4c5d039123769e9bcc63616f723a2bcd5
     let xdr =
-      'AAAAAAtjwtJadppTmm0NtAU99BFxXXfzPO1N/SqR43Z8aXqXAAAAZAAIj6YAAAACAAAAAAAAAAEAAAAB0QAAAAAAAAEAAAAAAAAAAQAAAADLa6390PDAqg3qDLpshQxS+uVw3ytSgKRirQcInPWt1QAAAAAAAAAAA1Z+AAAAAAAAAAABfGl6lwAAAEBC655+8Izq54MIZrXTVF/E1ycHgQWpVcBD+LFkuOjjJd995u/7wM8sFqQqambL0/ME2FTOtxMO65B9i3eAIu4P';
+      'AAAADBWheoe4LlAe+7qHTzO1YKan5UWXJcfm0lWUNHqhTmyyA+XiKtbbwlzS8tW1CtjdBTYgPZrdjpjJuV5zcl7PZVQAAAAAXujVJAAAAAAAAAAANP1gT8iyDhlsxmyQcoP2Fniod7mVjT2BTMVtkcjtn0Wgqpv9/HAz1qv3tHX358U3J8v/ft01KU1czAlKoGDFNQBgpwMAI4byb8EAAAAAAACJHeRgAAAAAAAAAAAAAAZhAADDUABMS0AAAAPog9syLpVvCsPfN31b22TfKiyG534TEFrxEghraQKQGTSuAj4KfMUigmu1l3k5dBaUB8NvVSnHLEgnA3/oU+QLaRWGmADZ9POhjNpc3qw20WsJUlORcwbrvCeQvODnlw2uRiJzcGSGYQLqV5qdBgsME1k1p2LB2Tkpi/8odwrQ5doAAAAA';
     var tx = new StellarBase.Transaction(xdr, StellarBase.Networks.PUBLIC);
     expect(tx.hash().toString('hex')).to.be.equal(
-      'a84d534b3742ad89413bdbf259e02fa4c5d039123769e9bcc63616f723a2bcd5'
+      'c27ba8968f29c7e684b7a5947f452100bb610996402b5cc75304180b07583248'
     );
     done();
   });
